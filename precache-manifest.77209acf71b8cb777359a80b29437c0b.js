8c3b34eeb8070780dc087eb08e0f69f0ec5fad12
walanpat <39136284+walanpat@users.noreply.github.com>
2020-07-24 21:00:27 -0400

    Updates

---
+++ precache-manifest.77209acf71b8cb777359a80b29437c0b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f3ab5d748f9ce947e94480ddd87aa5",
+    "url": "/walanpat.github.io/index.html"
+  },
+  {
+    "revision": "975b974264722c409370",
+    "url": "/walanpat.github.io/static/css/main.fd6ab39f.chunk.css"
+  },
+  {
+    "revision": "b9711659b8460c64e945",
+    "url": "/walanpat.github.io/static/js/2.7ae5c109.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/walanpat.github.io/static/js/2.7ae5c109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975b974264722c409370",
+    "url": "/walanpat.github.io/static/js/main.2ad87f22.chunk.js"
+  },
+  {
+    "revision": "e44a88ab724582b25ca5",
+    "url": "/walanpat.github.io/static/js/runtime-main.d8bfa98f.js"
+  },
+  {
+    "revision": "cc6cd9b244c3757f5bec2284329a13c4",
+    "url": "/walanpat.github.io/static/media/Eucharist Image.cc6cd9b2.jpg"
+  },
+  {
+    "revision": "53a5bd9fe95540a8e940ca4f16804454",
+    "url": "/walanpat.github.io/static/media/MyFace.53a5bd9f.jpg"
+  },
+  {
+    "revision": "dfd9de91818928d4b0917aabf63d82c5",
+    "url": "/walanpat.github.io/static/media/Ordinationstart.dfd9de91.jpg"
+  },
+  {
+    "revision": "19d1b87036d745d4b61ce1d93c5742c1",
+    "url": "/walanpat.github.io/static/media/Oxford.19d1b870.jpg"
+  },
+  {
+    "revision": "d660b1938c7fb98ca93de18736978c01",
+    "url": "/walanpat.github.io/static/media/Paperwork.d660b193.jpg"
+  },
+  {
+    "revision": "acb7d2e068aa77344e37347b069be37e",
+    "url": "/walanpat.github.io/static/media/Prostration.acb7d2e0.jpg"
+  },
+  {
+    "revision": "3556551e9619f11dfffa73da0d615737",
+    "url": "/walanpat.github.io/static/media/layingofhands.3556551e.jpg"
+  },
+  {
+    "revision": "1ca07ea190dcebf699f573b360a58cfe",
+    "url": "/walanpat.github.io/static/media/monks.1ca07ea1.jpg"
+  },
+  {
+    "revision": "5e094b96e0e64db0f217ae83dc34f082",
+    "url": "/walanpat.github.io/static/media/walkingtochurch.5e094b96.jpg"
+  }
+]);
